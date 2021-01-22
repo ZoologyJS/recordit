14377d6d10ae4ac247dea83dadcd8d595384e850
Chris <51548719+ZoologyJS@users.noreply.github.com>
2021-01-22 15:49:15 -0800

    first commit to heroku

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <!-- <div class="cont">
+        <v-color-picker
+          class="ma-2"
+          canvas-height="300"
+          @update:color="getColor"
+        ></v-color-picker>
+      </div> -->
+      <Container />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { debounce } from 'vue-debounce'
+import Container from './components/Container';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Container },
+  data() {
+    return {
+      // currColor: null
+    }
+  },
+  methods: {
+    // getColor: function(e) {
+    //     console.log(e)
+    //     const {r, g, b} = e.rgba;
+    //     this.currColor = `rgb(${r}, ${g},${b})`;
+    //     this.changeBody(this.currColor);
+    //     console.log(this.currColor)
+    // },
+    // changeBody: function(color) {
+    //   document.querySelector(".cont").style.backgroundColor = color;
+    // }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body > #app {
+    background-color: #1f2e42;
+  }
 </style>
---
+++ src/plugins/debounce.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import vueDebounce from 'vue-debounce';
+
+Vue.use(vueDebounce);
+
+export default new vueDebounce({
+});
